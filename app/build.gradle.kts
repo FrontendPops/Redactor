b384231813a0ea9a968b2471f2ccef27a8eb15f6
Хорев Павел Александрович <khorev05@list.ru>
2024-05-22 20:27:29 +0700

    bugfix commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
 
     implementation ("com.intuit.sdp:sdp-android:1.1.1")
     implementation("com.intuit.ssp:ssp-android:1.1.1")
